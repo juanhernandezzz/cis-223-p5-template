db5e9884622ba8ae0d07d585c68ae45f307bf9df
juanHernandezzz <juhern39@stu.jjc.edu>
2024-04-25 14:01:21 -0500

    canvas and square is made along with a soundimport

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
+
+let mySound;
+
+function preload(){
+    soundFormats('mp3','wav','ogg');
+    mySound = loadSound("bass.wav", right, wrong);
+}
 function setup() {
     createCanvas(400, 400);
-
+    
 }
 
-function draw() {
-    background(120);
-}
+ function draw() {
+   
+    background(50);
+    square(50, 50, 50);
+    
+ }
+
+
+
+ function b1pressed(){
+     mySound.play();
+ }
+
+ 
+ function right(){
+    alert("it works");
+ }
+
+ function wrong(){
+    alert("does not work");
+ }
